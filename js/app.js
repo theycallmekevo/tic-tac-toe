33a618f2e5f096930c6248b6623da5c6b0285b01
Kevin Tran <kevin2005tran@gmail.com>
2016-10-28 16:28:48 -0700

    successfully manipulated the DOM

--- js/app.js
+++ js/app.js
@@ -1,6 +1,62 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
+
+//Reloads page when user clicks on reset button
+$('button').click(function() {
+    location.reload();
+});
+
+//variables of user clicks on each box
+
+var topLeft = $('#topLeft').click(function(){
+  console.log("you won!");
+  });
+
+
+// var topLeft = $('#topLeft').click(function(){
+//     $(this).css({'background-image': 'url(images/ex.jpg)',
+//                  'background-position': 'center',
+//                  'background-size': 'cover'});
+//               });
+
+var topMiddle = $('#topMiddle').click(function(){
+    $(this).css('background-color', '#0099cc');
+});
+
+var topRight = $('#topRight').click(function(){
+    $(this).css('background-color', '#ffdab9');
+});
+
+var middleLeft = $('#middleLeft').click(function(){
+    $(this).css('background-color', '#6897bb');
+});
+
+var middleMiddle = $('#middleMiddle').click(function(){
+    $(this).css('background-color', '#ffb6c1');
+});
+
+var middleRight = $('#middleRight').click(function(){
+    $(this).css('background-color', '#e6e6fa');
+});
+
+var bottomLeft = $('#bottomLeft').click(function(){
+    $(this).css('background-color', '#ffd700');
+});
+
+var bottomMiddle = $('#bottomMiddle').click(function(){
+    $(this).css('background-color', '#00ff7f');
+});
+
+var bottomRight = $('#bottomRight').click(function(){
+    $(this).css('background-color', '#990000');
+});
+
+
+
+
+
+
+
+
 
 });
