82fc8f6d8603926980c7d86969262863080b1ea5
Kevin Tran <kevin2005tran@gmail.com>
2017-01-01 19:12:27 -0800

    cleaned up win scenarios

--- js/app.js
+++ js/app.js
@@ -1,17 +1,10 @@
-// wait for the DOM to finish loading
 $(document).ready(function() {
-//Reloads page when user clicks on reset button
-$('button').click(function() {
-    location.reload();
-});
+$('button').click(function() { location.reload(); });
 
-//pair programming team: Kyle Russell & Kevin Tran
-//game counter (global variable: will update after each user click)
 var gameCounter = 0;
-//created function to start game when user clicks
+
 $('.box').click(function(){
-//creating game win conditions
-//ways for player x to win
+    //ways for player x to win
     var xWin1 = ($('#topLeft').text() === 'X' && $('#topMiddle').text() === 'X' && $('#topRight').text() === 'X');
     var xWin2 = ($('#middleLeft').text() === 'X' && $('#middleMiddle').text() === 'X' && $('#middleRight').text() === 'X');
     var xWin3 = ($('#bottomLeft').text() === 'X' && $('#bottomMiddle').text() === 'X' && $('#bottomRight').text() === 'X');
@@ -20,7 +13,7 @@ $('.box').click(function(){
     var xWin6 = ($('#topRight').text() === 'X' && $('#middleRight').text() === 'X' && $('#bottomRight').text() === 'X');
     var xWin7 = ($('#topLeft').text() === 'X' && $('#middleMiddle').text() === 'X' && $('#bottomRight').text() === 'X');
     var xWin8 = ($('#topRight').text() === 'X' && $('#middleMiddle').text() === 'X' && $('#bottomLeft').text() === 'X');
-//ways for player O to win
+    //ways for player O to win
     var oWin1 = ($('#topLeft').text() === 'O' && $('#topMiddle').text() === 'O' && $('#topRight').text() === 'O');
     var oWin2 = ($('#middleLeft').text() === 'O' && $('#middleMiddle').text() === 'O' && $('#middleRight').text() === 'O');
     var oWin3 = ($('#bottomLeft').text() === 'O' && $('#bottomMiddle').text() === 'O' && $('#bottomRight').text() === 'O');
@@ -29,10 +22,10 @@ $('.box').click(function(){
     var oWin6 = ($('#topRight').text() === 'O' && $('#middleRight').text() === 'O' && $('#bottomRight').text() === 'O');
     var oWin7 = ($('#topLeft').text() === 'O' && $('#middleMiddle').text() === 'O' && $('#bottomRight').text() === 'O');
     var oWin8 = ($('#topRight').text() === 'O' && $('#middleMiddle').text() === 'O' && $('#bottomLeft').text() === 'O');
-//adding all ways to win into one variable each
+    //adding all ways to win into one variable each
     var xWins = (xWin1 || xWin2 || xWin3 || xWin4 || xWin5 || xWin6 || xWin7 || xWin8);
     var oWins = (oWin1 || oWin2 || oWin3 || oWin4 || oWin5 || oWin6 || oWin7 || oWin8);
-//game play switching from X to O on even and odd values of game counter. Game counter set to 0, on even = X, on odd = O. Updates gamecounter after each user click.
+    //game play switching from X to O on even and odd values of game counter. Game counter set to 0, on even = X, on odd = O. Updates gamecounter after each user click.
     if ($(this).html() === '') {
     for (var i = 0; i < 1; i++) {
       if (gameCounter % 2 === 0) {
@@ -44,7 +37,7 @@ $('.box').click(function(){
         }
       }
     }
-//checks if condition is true and alerts user of Winner!
+    //checks if condition is true and alerts user of Winner!
     if(xWins){
       alert('Player X wins!');
       return;
@@ -54,59 +47,4 @@ $('.box').click(function(){
     } else {
   }
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//setting all game boxes to variable
-// var box1 = $('#topLeft').click(function(){
-  // console.log('box 1');
-  // $(this).css('background-color', 'red');
-  // $(this).html('X');
-  // $(this).text('O');
-// });
-// var box2 = $('#topMiddle').click(function(){
-//   console.log('box 2');
-// });
-// var box3 = $('#topRight').click(function(){
-//   console.log('box 3');
-// });
-// var box4 = $('#middleLeft').click(function(){
-//   console.log('box 4');
-// });
-// var box5 = $('#middleMiddle').click(function(){
-//   console.log('box 5');
-// });
-// var box6 = $('#middleRight').click(function(){
-//   console.log('box 6');
-// });
-// var box7 = $('#bottomLeft').click(function(){
-//   console.log('box 7');
-// });
-// var box8 = $('#bottomMiddle').click(function(){
-//   console.log('box 8');
-// });
-// var box9 = $('#bottomRight').click(function(){
-//   console.log('box 9');
-// });
-
-
 });
